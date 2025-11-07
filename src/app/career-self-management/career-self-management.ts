9396d395bf163ddcc4ff6671883bf4ac253bc23c
IvoDabove <ivomartindabove96@gmail.com>
2025-11-07 18:08:40 -0300

    CareerSelfMangementChanges

--- src/app/career-self-management/career-self-management.ts
+++ src/app/career-self-management/career-self-management.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-import {  OnInit, ViewChild } from '@angular/core'; // Añadido OnInit, ViewChild
-import {  MatTableDataSource } from '@angular/material/table'; // MatTableDataSource aquí
-import { MatSort, MatSortModule } from '@angular/material/sort'; // MatSort aquí
-import { MatIconModule } from '@angular/material/icon'; // Para los íconos (edit, delete, add)
-import { MatButtonModule } from '@angular/material/button'; // Para los botones
-import { MatFormFieldModule } from '@angular/material/form-field'; // Para el campo de búsqueda
+import {  OnInit, ViewChild } from '@angular/core'; 
+import {  MatTableDataSource } from '@angular/material/table'; 
+import { MatSort, MatSortModule } from '@angular/material/sort'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatInputModule } from '@angular/material/input';
 export interface Carrera {
   id: number; 
   nombre: string;
-  materias: number; // O podrías usar string
+  materias: string; 
   descripcion: string;
   duracion: number; 
 }
 
 const DATOS_CARRERAS: Carrera[] = [
-  {id: 1, nombre: 'Ing. Sistemas', materias: 40, descripcion: 'Análisis y desarrollo de software', duracion: 5},
-  {id: 2, nombre: 'Arquitectura', materias: 35, descripcion: 'Diseño y construcción de espacios', duracion: 4},
-  // ... más datos
+  {id: 1, nombre: 'Ing. Sistemas', materias: 'Ver Materias', descripcion: 'Análisis y desarrollo de software', duracion: 5},
+  {id: 2, nombre: 'Arquitectura', materias: 'Ver Materias', descripcion: 'Diseño y construcción de espacios', duracion: 4},
+  
 ];
 
 @Component({
