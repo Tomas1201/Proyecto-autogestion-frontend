67c4743a09718bc168b9ec374e3614a763a25e4b
Tomas Acosta <tomasacosta53@protonmail.com>
2025-11-09 19:45:52 -0300

    restructuring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { MainView } from './main-view/main-view';
-import { CareerSelfManagement } from './career-self-management/career-self-management';
-import { ProfessorSelfManagement } from './professor-self-management/professor-self-management';
-import { StudentSelfManagement } from './student-self-management/student-self-management';
-import { SubjectSelfManagement } from './subject-self-management/subject-self-management';
+import { MainView } from './admin-panel/main-view/main-view';
+import { CareerSelfManagement } from './admin-panel/career-self-management/career-self-management';
+import { ProfessorSelfManagement } from './admin-panel/professor-self-management/professor-self-management';
+import { StudentSelfManagement } from './admin-panel/student-self-management/student-self-management';
+import { SubjectSelfManagement } from './admin-panel/subject-self-management/subject-self-management';
 
 export const routes: Routes = [{path:'', redirectTo:'/MainView', pathMatch:'full'},
 
